28e2d2413269a1c5f1f4e719124af79e803766a3
nguyenhuutien93 <nguyenhuutienk56a3@yahoo.com>
2016-05-21 23:39:28 +0700

    bay gio co the lam viec voi doan van ban Tieng Viet

--- string.php
+++ string.php
@@ -2,6 +2,7 @@
 <html>
 <head>
 	<title>Phân tích đoạn văn</title>
+	<meta charset="UTF-8">
 	<style type="text/css">
 		p {
 			float: left;
@@ -14,16 +15,29 @@
 	</style>
 </head>
 <body>
-<div>
-	<form action="string.php" method="post">
-		<textarea name="passage" rows="20" cols="100"></textarea><br><br>
-		<input type="text" name="letter"></input>
-		<input type="submit" name="analysis" value="Phân tích"></input>
-	</form>
-</div>
 </body>
 </html>
 <?php
+	function chuyenChuoi($str) {
+		// In thường
+	     $str = preg_replace("/(à|á|ạ|ả|ã|â|ầ|ấ|ậ|ẩ|ẫ|ă|ằ|ắ|ặ|ẳ|ẵ)/", 'a', $str);
+	     $str = preg_replace("/(è|é|ẹ|ẻ|ẽ|ê|ề|ế|ệ|ể|ễ)/", 'e', $str);
+	     $str = preg_replace("/(ì|í|ị|ỉ|ĩ)/", 'i', $str);
+	     $str = preg_replace("/(ò|ó|ọ|ỏ|õ|ô|ồ|ố|ộ|ổ|ỗ|ơ|ờ|ớ|ợ|ở|ỡ)/", 'o', $str);
+	     $str = preg_replace("/(ù|ú|ụ|ủ|ũ|ư|ừ|ứ|ự|ử|ữ)/", 'u', $str);
+	     $str = preg_replace("/(ỳ|ý|ỵ|ỷ|ỹ)/", 'y', $str);
+	     $str = preg_replace("/(đ)/", 'd', $str);    
+		// In đậm
+	     $str = preg_replace("/(À|Á|Ạ|Ả|Ã|Â|Ầ|Ấ|Ậ|Ẩ|Ẫ|Ă|Ằ|Ắ|Ặ|Ẳ|Ẵ)/", 'A', $str);
+	     $str = preg_replace("/(È|É|Ẹ|Ẻ|Ẽ|Ê|Ề|Ế|Ệ|Ể|Ễ)/", 'E', $str);
+	     $str = preg_replace("/(Ì|Í|Ị|Ỉ|Ĩ)/", 'I', $str);
+	     $str = preg_replace("/(Ò|Ó|Ọ|Ỏ|Õ|Ô|Ồ|Ố|Ộ|Ổ|Ỗ|Ơ|Ờ|Ớ|Ợ|Ở|Ỡ)/", 'O', $str);
+	     $str = preg_replace("/(Ù|Ú|Ụ|Ủ|Ũ|Ư|Ừ|Ứ|Ự|Ử|Ữ)/", 'U', $str);
+	     $str = preg_replace("/(Ỳ|Ý|Ỵ|Ỷ|Ỹ)/", 'Y', $str);
+	     $str = preg_replace("/(Đ)/", 'D', $str);
+	     return $str; // Trả về chuỗi đã chuyển
+	}  
+   
 	function standarzedStr($str) {
 		$str = trim($str);
 		$len = strlen($str);
@@ -41,20 +55,34 @@
 		$str = substr($str, 0, $len-$count);	
 		return $str;
 	}
-
+	$str="";
+	$let="";
 	if(isset($_POST['analysis'])) {
 		$str = $_POST['passage'];
 		//echo "[".$str."]"."<br>";
-		$str = standarzedStr($str);
-		
+		//for($i=0; $i < strlen($str); $i++)
+			//echo $str[$i]." ";
 		$let = $_POST['letter'];
+	}
+?>
+<div>
+	<form action="string.php" method="post">
+		<textarea name="passage" rows="20" cols="100"><?php echo $str?></textarea><br><br>
+		<input type="text" name="letter" value="<?php echo $let?>"></input>
+		<input type="submit" name="analysis" value="Phân tích" ></input>
+	</form>
+</div>
+<?php
+	if(isset($_POST['analysis'])) {
+		
 		if(strlen($let) > 1 || $let == "")
 			echo "Hãy nhập 1 kí tự";
 		else {
 			$count = 0;
-			$len = strlen($str);
+			$temp = chuyenChuoi($str);
+			$len = strlen($temp);
 			for($i=0; $i < $len; $i++)
-				if($str[$i] == $let || $str[$i] == strtoupper($let) || $str[$i] == strtolower($let))
+				if($temp[$i] == $let || $temp[$i]  == strtoupper($let) || $temp[$i] == strtolower($let))
 					$count++;
 			echo "Có ".$count." kí tự ".$let."<br>";
 			if($count > 0) {
@@ -63,7 +91,7 @@
 				//echo count($word);
 				echo "Các từ có chứa kí tự '".$let."':<br>";
 				for($i=0;$i<count($word);$i++) {
-					if(strpos($word[$i], $let)!==FALSE || strpos($word[$i], strtoupper($let))!==FALSE || strpos($word[$i], strtolower($let))!==FALSE){
+					if(strpos(chuyenChuoi($word[$i]), $let)!==FALSE || strpos(chuyenChuoi($word[$i]), strtoupper($let))!==FALSE || strpos(chuyenChuoi($word[$i]), strtolower($let))!==FALSE){
 						//echo strpos($word[$i], $let);
 						echo $word[$i]. " ";
 					}
@@ -71,13 +99,17 @@
 				echo "<br>";
 				for ($i=0;$i<count($word);$i++) { 
 					
-					if(strpos($word[$i], $let)!==FALSE || strpos($word[$i], strtoupper($let))!==FALSE || strpos($word[$i], strtolower($let))!==FALSE){
+					if(strpos(chuyenChuoi($word[$i]), $let)!==FALSE || 
+						strpos(chuyenChuoi($word[$i]), strtoupper($let))!==FALSE || 
+						strpos(chuyenChuoi($word[$i]), strtolower($let))!==FALSE){
 						echo "<p class='word'>";
 						echo $word[$i]." ";
 						echo "</p>";
 					} else
 
-					if(strpos($word[$i], $let)===FALSE || strpos($word[$i], strtoupper($let))===FALSE || strpos($word[$i], strtolower($let))===FALSE){
+					if(strpos(chuyenChuoi($word[$i]), $let)===FALSE || 
+						strpos(chuyenChuoi($word[$i]), strtoupper($let))===FALSE || 
+						strpos(chuyenChuoi($word[$i]), strtolower($let))===FALSE){
 						echo "<p>";
 						echo $word[$i]." ";
 						echo "</p>";
